868c4788ebe98efc65732cb63d49a07690e91bff
Abhijeet Khadake <akhadake@outlook.com>
2020-01-17 01:31:31 +0530

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace helloWorldAPI.Controllers
+{
+    public class HelloController : ApiController
+    {
+        public string GetHello()
+        {
+            return "<h3>Hello Abhijeet</h3>";
+        }
+    }
+}
